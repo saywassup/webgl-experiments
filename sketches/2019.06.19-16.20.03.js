469b4da8655326f062aa2734aab2c7191ae7b60c
John <unidospelosoftware@outlook.com>
2019-06-25 20:02:21 -0300

    new: drawing a geometry with buffers

--- sketches/2019.06.19-16.20.03.js
+++ sketches/2019.06.19-16.20.03.js
@@ -1,10 +1,3 @@
-//Learn resources
-//http://jsfiddle.net/w67tzfhx/
-//https://stackoverflow.com/questions/31399856/drawing-a-line-with-three-js-dynamically/31411794#31411794
-//https://threejs.org/docs/#api/en/core/BufferGeometry
-//https://threejs.org/docs/#api/en/core/BufferAttribute
-//http://wtf.tw/ref/shiffman.pdf
-//https://stackoverflow.com/questions/42229799/how-to-smoothly-animate-drawing-of-a-line/42236893#42236893
 
 const canvasSketch = require('canvas-sketch');
 
@@ -15,27 +8,12 @@ require('three/examples/js/controls/OrbitControls');
 const settings = {
   animate: true,
   context: 'webgl',
-  attributes: { antialias: false }
+  attributes: { antialias: true }
 };
 
-const Walker = () => ({
-  x: window.visualViewport.width / 2,
-  y: window.visualViewport.height / 2,
-  z: 0
-});
+let fraction = 0;
+let lineLength;
 
-const walkerMethods = {
-  display() {
-    // criar uma linha
-
-    // criar um ponto
-    this.x;
-    this.y;
-  },
-  step() {
-    const choice = 0;
-  }
-};
 
 const sketch = ({ context }) => {
   // Create a renderer
@@ -44,11 +22,11 @@ const sketch = ({ context }) => {
   });
 
   // WebGL background color
-  renderer.setClearColor('#fff', 1);
+  renderer.setClearColor('#1E1B23', 1);
 
   // Setup a camera
   const camera = new THREE.PerspectiveCamera(45, 1, 0.01, 100);
-  camera.position.set(2, 2, -4);
+  camera.position.set(0, 0, -50);
   camera.lookAt(new THREE.Vector3());
 
   // Setup camera controller
@@ -57,27 +35,56 @@ const sketch = ({ context }) => {
   // Setup your scene
   const scene = new THREE.Scene();
 
-  var material = new THREE.LineBasicMaterial({
-    color: 0x000000
-  });
-  
-  var geometry = new THREE.Geometry();
-  geometry.vertices.push(
-    new THREE.Vector3( 10, 0, 0 ),
-    new THREE.Vector3( 0, 10, 5 ),
-    new THREE.Vector3( 0, -15, -15 )
-  );
-  
-  var line = new THREE.Line( geometry, material );
-  scene.add( line );
+  // points
+	let points = ( new THREE.TorusKnotGeometry( 10, 3, 300, 300 ) ).vertices;
 
-  geometry.vertices.push(
-    new THREE.Vector3( -10, -0.5, -25 )
-  );
+	// geometry
+	let geometry = new THREE.BufferGeometry();
 
-  geometry.vertices.push(
-    new THREE.Vector3( 0, -10, -5 )
-  );
+	// attributes
+	numPoints = points.length;
+	let positions = new Float32Array( numPoints * 3 );
+	let colors = new Float32Array( numPoints * 3 );
+	let lineDistances = new Float32Array( numPoints * 1 );
+
+	geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+	geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
+	geometry.addAttribute( 'lineDistance', new THREE.BufferAttribute( lineDistances, 1 ) );
+
+	// populate
+	let color = new THREE.Color();
+
+	for ( let i = 0, index = 0, l = numPoints; i < l; i ++, index += 3 ) {
+
+		positions[ index ] = points[ i ].x;
+		positions[ index + 1 ] = points[ i ].y;
+		positions[ index + 2 ] = points[ i ].z;
+
+		color.setHSL(1,1,1);
+
+		colors[ index ] = color.r;
+		colors[ index + 1 ] = color.g;
+		colors[ index + 2 ] = color.b;
+
+		if ( i > 0 ) {
+			lineDistances[ i ] = lineDistances[ i - 1 ] + points[ i - 1 ].distanceTo( points[ i ] );
+		}
+
+	}
+
+	lineLength = lineDistances[ numPoints - 1 ];
+
+	// material
+	let material = new THREE.LineDashedMaterial( {
+  	vertexColors: THREE.VertexColors,
+  	dashSize: 1,
+  	gapSize: 1e10
+	});
+
+	// line
+	line = new THREE.Line( geometry, material );
+	scene.add( line );
+
 
   // Specify an ambient/unlit colour
   scene.add(new THREE.AmbientLight('#59314f'));
@@ -99,6 +106,12 @@ const sketch = ({ context }) => {
     // Update & render your scene here
     render ({ time }) {
       // mesh.rotation.y = time * (10 * Math.PI / 180);
+      
+      if(fraction < 0.9999) {
+        fraction = ( fraction + 0.001 ) % 1;
+	      line.material.dashSize = fraction * lineLength;
+      }
+
       controls.update();
       renderer.render(scene, camera);
     },
